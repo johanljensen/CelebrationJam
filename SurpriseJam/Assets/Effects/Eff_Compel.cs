994f4a1f3f95fbb9e12b59e1dfc50e873ab99230
JohanLJensen <johanljensen@hotmail.com>
2025-03-16 11:28:57 +0100

    Compel effect
    Speedboost with a light to indicate which enemy to be scared of

--- SurpriseJam/Assets/Effects/Eff_Compel.cs
+++ SurpriseJam/Assets/Effects/Eff_Compel.cs
@@ -3,22 +3,63 @@ using UnityEngine.EventSystems;
 
 public class Eff_Compel : Effect
 {
+    [SerializeField] private float duration;
+    [SerializeField] private float speedMultiplier;
+
+    private Transform effectTarget;
+    private bool isPlayerFriendly;
+    private float durationTimer;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        durationTimer = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        durationTimer += Time.deltaTime;
+
+        if (durationTimer >= duration)
+        {
+            SetSpeedMod(1);
+            Destroy(gameObject);
+        }
     }
 
-    public string EffectName { get; }
+    private void SetSpeedMod(float speedMod)
+    {
+        if (effectTarget == null)
+        { return; }
+        
+        if (isPlayerFriendly)
+        {
+            PlayerController player = effectTarget.GetComponent<PlayerController>();
+            if (player != null)
+            {
+                player.ChangeSpeedAdjustment(speedMod);
+            }
+        }
+        else
+        {
+            
+            EnemyEyEye enemy = effectTarget.GetComponent<EnemyEyEye>();
+            if (enemy != null)
+            {
+                enemy.ChangeSpeedAdjustment(speedMod);
+            }
+        }
+    }
 
     public override void ActivateEffect(bool playerFriendly, Transform activatorCharacter, Vector3 wheelPosition)
     {
-        Debug.LogWarning(GetType());
+        isPlayerFriendly = playerFriendly;
+        effectTarget = activatorCharacter;
+        
+        transform.parent = effectTarget;
+        transform.localPosition = Vector3.zero;
+        
+        SetSpeedMod(speedMultiplier);
     }
 }
