765c966d416f5ade140cc67a8eafc682308d8122
JohanLJensen <johanljensen@hotmail.com>
2025-03-16 10:40:59 +0100

    Grave effect
    Now spawns an annoying gravestone to make pathfinding hell

--- SurpriseJam/Assets/Effects/Eff_Gravestone.cs
+++ SurpriseJam/Assets/Effects/Eff_Gravestone.cs
@@ -2,22 +2,10 @@ using UnityEngine;
 
 public class Eff_Gravestone : Effect
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    public string EffectName { get; }
-
+    [SerializeField] private GameObject gravePrefab;
+    
     public override void ActivateEffect(bool playerFriendly, Transform activatorCharacter, Vector3 wheelPosition)
     {
-        Debug.LogWarning(GetType());
+        transform.position = wheelPosition;
     }
 }
